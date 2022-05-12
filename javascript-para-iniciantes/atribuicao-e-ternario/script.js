61de82fdec235ab9d8268a57101daae9b0de2c49
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-12 18:05:42 -0300

    atribuição e ternário

---
+++ javascript-para-iniciantes/atribuicao-e-ternario/script.js
@@ -0,0 +1,13 @@
+var numero = 20
+numero+=10
+console.log(numero)
+
+var idade = 20
+var podeBeber = (idade >= 18)? 'Pode beber' : 'Não pode beber'
+
+console.log(podeBeber)
+
+var possuiCarro = true;
+var possuiCasa = true;
+darCredito = (possuiCarro && possuiCasa)
+console.log(darCredito)
