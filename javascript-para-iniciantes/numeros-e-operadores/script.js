4a97443040a1cce6a593af0f0440ca50ae2d39e0
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-12 13:27:07 -0300

    operadores e números

---
+++ javascript-para-iniciantes/numeros-e-operadores/script.js
@@ -0,0 +1,39 @@
+var total = 10+ 5 + 10;
+var divisao = 200/5;
+var modulo =326724516734567145%3;
+
+console.log(total);
+console.log(divisao);
+console.log(modulo);
+
+var testeNaN = 'isso é 100' / 100;
+console.log(testeNaN);
+
+var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+console.log(soma1);
+
+var incremento = 1;
+console.log(incremento++);
+console.log(incremento);
+
+var idade = '20';
+var somaIdade = 5;
+
+console.log(+idade + 5);
+
+var ex1 = 10+5*2/2+20;
+console.log(ex1);
+
+var nan1 = 'teste NaN 4'/2;
+var nan2 = 'teste NaN 10'/5;
+console.log(nan1, nan2);
+
+console.log(+'200'+50);
+
+var incremento2 = 5;
+console.log(incremento2++);
+console.log(incremento2);
+
+var peso = '80kg';
+
+console.log(+peso.slice(0,2)/2)
