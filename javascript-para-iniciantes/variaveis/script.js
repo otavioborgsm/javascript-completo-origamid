adb9601a6675db4abc51133487329398da58d437
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-12 10:44:09 -0300

    aula de variáveis

---
+++ javascript-para-iniciantes/variaveis/script.js
@@ -0,0 +1,32 @@
+var nome = 'Otávio';
+var idade = 20;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Borges', 
+    cidade ='Três Cachoeiras'
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+var comida
+console.log(comida);
+comida = 'Pizza'
+console.log(comida);
+
+var time = 'Grêmio';
+time = 'Brasil'
+console.log(time);
+
+//Declare 5 variáveis sem valores
+
+var curso, empregoAtual, empregado, endereco, formacao
