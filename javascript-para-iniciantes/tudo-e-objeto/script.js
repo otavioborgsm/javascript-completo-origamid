32189146ab5b321ee9fb47f5faa4a5fca9028d80
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-12 16:40:20 -0300

    tudo é objeto

---
+++ javascript-para-iniciantes/tudo-e-objeto/script.js
@@ -0,0 +1,9 @@
+var nome = 'Otávio'
+nome.replace('io', 'inho')
+console.log(nome)
+console.log(nome.replace('io', 'inho'))
+console.log(nome.length)
+console.log(nome.toLowerCase())
+
+var btn = document.querySelector('.btn')
+
