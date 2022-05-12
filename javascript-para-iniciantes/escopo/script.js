a6b6a5981f434b393d34931b4832ec69e16c8e7e
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-12 18:26:03 -0300

    escopo

---
+++ javascript-para-iniciantes/escopo/script.js
@@ -0,0 +1,19 @@
+var carro = 'Fusca'
+
+function mostrarCarro() {
+    console.log(carro)
+}
+mostrarCarro()
+
+if (true) {
+    let mes='dezembro'
+    console.log(mes)
+}
+
+{
+    var carro2='Kombi'
+    const ano = 2022
+}
+console.log(carro2)
+//console.log(ano)
+
