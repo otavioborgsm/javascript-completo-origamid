07967873d07ae9db8f35c0acc0d75396e529aba0
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-12 11:34:08 -0300

    tipos de dados

---
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -0,0 +1,27 @@
+var nome = 'Otávio';
+var idade = 20;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var sobrenome = 'Borges';
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'Romário fez ' + gols + ' gols'
+console.log(typeof frase)
+
+var ano = '2018';
+var mes = 8;
+console.log(ano+mes);
+
+var frase1 = "Esse é o \"Melhor\" jogo";
+
+console.log(frase1);
+
+var frase2 = `Romário fez ${gols*2} gols`;
+console.log(frase2);
+
+var frase3 = 'It\'s time'
+console.log(frase3)
+console.log(typeof nome)
