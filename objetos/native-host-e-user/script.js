01bbf67827555416c3ae6c48c203d9bb7d387754
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-18 15:06:17 -0300

    objetos nativos, do browser ou do usuário

---
+++ objetos/native-host-e-user/script.js
@@ -0,0 +1,31 @@
+
+if (typeof Array.from !== 'undefined') {
+    console.log('Existe')
+}else{
+    console.log('Não existe')
+}
+
+// Liste 5 objetos nativos
+
+/*
+Object
+String
+Array
+Function
+Number
+*/
+
+// Liste 5 objetos do browser
+
+/*
+Window
+history
+Document
+HTMLCOllection
+NodeList
+*/
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+//webkitVisibilityState
