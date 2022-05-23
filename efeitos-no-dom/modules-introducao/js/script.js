bb457e66d6825b970f18aef2419242d406a92a74
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-22 23:23:20 -0300

    oraganizando o projeto em modules

---
+++ efeitos-no-dom/modules-introducao/js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from "./modules/scroll-suave";
+import initAnimacaoScroll from './modules/scroll-animacao'
+import initAccordion from './modules/accordion'
+import initTabNav from './modules/tabnav'
+
+initAccordion();
+initAnimacaoScroll();
+initScrollSuave();
+initTabNav();
