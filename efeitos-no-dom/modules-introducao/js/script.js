0c0f7c415921c410742f8da1348f9561ecdf06b9
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-23 10:00:49 -0300

    organizando o projeto em modules

--- efeitos-no-dom/modules-introducao/js/script.js
+++ efeitos-no-dom/modules-introducao/js/script.js
@@ -1,7 +1,7 @@
-import initScrollSuave from "./modules/scroll-suave";
-import initAnimacaoScroll from './modules/scroll-animacao'
-import initAccordion from './modules/accordion'
-import initTabNav from './modules/tabnav'
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+import initAccordion from './modules/accordion.js'
+import initTabNav from './modules/tabnav.js'
 
 initAccordion();
 initAnimacaoScroll();
