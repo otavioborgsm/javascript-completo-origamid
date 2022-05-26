2d7cb73e2a45ae42ad7b30deee9d220bbaa5996a
Otávio Borges <otavio.demb@rede.ulbra.br>
2022-05-26 18:35:57 -0300

    destructuring

---
+++ mais-javascript/destructuring/script.js
@@ -0,0 +1,22 @@
+// Extraia o backgroundColor, color e margin do btn
+const btn = document.querySelector('button');
+const btnStyles = getComputedStyle(btn);
+
+const {backgroundColor ,color, margin} = btnStyles
+console.log(backgroundColor ,color, margin)
+
+// Troque os valores das variáveis abaixo
+let cursoAtivo = 'JavaScript';
+let cursoInativo = 'HTML';
+
+[cursoAtivo, cursoInativo] = [cursoInativo, cursoAtivo]
+
+// Corrija o erro abaixo
+const cachorro = {
+  nome: 'Bob',
+  raca: 'Labrador',
+  cor: 'Amarelo'
+}
+
+//const {bobCor: cor} = cachorro;
+const {cor: bobcor} = cachorro;
